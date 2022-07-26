3daf9880c655b676b65aee398d0523d25a24b367
germanium-git <germanium-git@gmx.com>
2022-07-26 16:22:58 +0200

    path to filename

--- tf/configfile.tf
+++ tf/configfile.tf
@@ -1,5 +1,6 @@
 # Save the k8s config into the file
 resource "local_file" "kubeconfig" {
   content  = azurerm_kubernetes_cluster.main.kube_config_raw
-  filename = "kubeconfig"
+  filename = "../k8s_config/kubeconfig"
+
 }
